d3aa704d88e2432c1e47f9dbc97104b1a81fdda6
Steve Kamerman <stevekamerman@gmail.com>
2020-02-19 09:38:27 -0500

    Added config for proposed change

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
 
 module.exports = {
-    // Use the ImageEngine to serve our assets in production
+    // Use ImageEngine to serve our assets in production
     publicPath: process.env.NODE_ENV === 'production'
         ? 'https://lxrue9qg.cdn.imgeng.in/'
-        : '/'
+        : '/',
+
+    // This config is waiting on file-loader support for [query]
+    // @see https://github.com/webpack-contrib/file-loader/issues/364
+    // chainWebpack: config => {
+    //     config.module.rule('images')
+    //         .use('url-loader')
+    //         .tap(options => {
+    //             // options.fallback is file-loader
+    //             options.fallback.options.name += '[query]'
+    //             return options
+    //         })
+    // },
 }
