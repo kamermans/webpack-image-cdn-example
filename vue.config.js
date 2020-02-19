8be13a3c94230ce12e1920f1ddb37c8ca4baebf6
Steve Kamerman <stevekamerman@gmail.com>
2020-02-19 09:02:59 -0500

    Used a real CDN config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@
 module.exports = {
     // Use the ImageEngine to serve our assets in production
     publicPath: process.env.NODE_ENV === 'production'
-        ? 'https://cdn.stevekamerman.com/https://distracted-ritchie-1f0417.netlify.com/'
+        ? 'https://lxrue9qg.cdn.imgeng.in/'
         : '/'
 }
