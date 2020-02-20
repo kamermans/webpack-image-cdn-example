a4aaa2c0f705e3ff4b6a7bd40d19727d7266136b
Steve Kamerman <stevekamerman@gmail.com>
2020-02-20 17:08:04 -0500

    Added CDN support

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,23 @@
+isProduction = process.env.NODE_ENV === 'production'
+cdn = 'https://lxrue9qg.cdn.imgeng.in/'
 
 module.exports = {
     // Use ImageEngine to serve our assets in production
-    publicPath: process.env.NODE_ENV === 'production'
-        ? 'https://lxrue9qg.cdn.imgeng.in/'
-        : '/',
+    publicPath: isProduction ? cdn : '/',
 
-    // This config is waiting on file-loader support for [query]
+    // This config requires file-loader v5.1.0+
     // @see https://github.com/webpack-contrib/file-loader/issues/364
-    // chainWebpack: config => {
-    //     config.module.rule('images')
-    //         .use('url-loader')
-    //         .tap(options => {
-    //             // options.fallback is file-loader
-    //             options.fallback.options.name += '[query]'
-    //             return options
-    //         })
-    // },
+    chainWebpack: config => {
+        if (isProduction) {
+            config.module.rule('images')
+                .use('url-loader')
+                .tap(options => {
+                    // options.fallback is file-loader
+                    options.fallback.loader = require.resolve('file-loader')
+                    options.fallback.options.name += '[query]'
+                    return options
+                })
+        }
+    },
+
 }
